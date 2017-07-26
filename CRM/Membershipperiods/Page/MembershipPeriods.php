e390626fd00edac0da0d19f3ee406013ef93e260
thebrentc <thebrentc@gmail.com>
2017-07-26 16:29:46 +0100

    Code tidy

--- CRM/Membershipperiods/Page/MembershipPeriods.php
+++ CRM/Membershipperiods/Page/MembershipPeriods.php
@@ -3,11 +3,8 @@
 class CRM_Membershipperiods_Page_MembershipPeriods extends CRM_Core_Page {
 
   public function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(ts('MembershipPeriods'));
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));    
+    CRM_Utils_System::setTitle(ts('MembershipPeriods'));
 
     // get contact id from $_GET ???
     // validate
@@ -15,10 +12,6 @@ class CRM_Membershipperiods_Page_MembershipPeriods extends CRM_Core_Page {
       die("Validation error"); // TODO better handling
     }
     $this->assign('cid', $_GET['cid']); 
-
-    // compute civicrm membership url for links on membership periods tab    
-    $membership_url = CRM_Utils_System::url('civicrm/contact/view/membership');
-    $this->assign('membership_url', $membership_url);
 
     // get membership periods data for contact
     $data = CRM_Membershipperiods_BAO_MembershipPeriod::retrieve($_GET['cid']);        
